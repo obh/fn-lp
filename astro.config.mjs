bf9346e5cb4000bb2286c89cd572140e5bc4598a
obh <rsedwardian@gmail.com>
2024-08-21 01:49:31 -0700

    removing base

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,5 @@ import { defineConfig } from "astro/config";
 // https://astro.build/config
 export default defineConfig({
   site: "https://flowwise.pages.github.io",
-  base: "fn-lp",
   integrations: [tailwind(), icon()]
 });
